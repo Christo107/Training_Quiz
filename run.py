e368b734d1ded08eca47f4c63d547ea07ce7fa85
Christo107 <christopher.odwyer82@gmail.com>
2022-11-26 12:24:37 +0000

    Add quiz data to run.py

--- run.py
+++ run.py
@@ -12,8 +12,32 @@ SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('chris_python_quiz')
 
-results = SHEET.worksheet('results')
-
-data = results.get_all_values()
-
-print(data)
+quiz_data = [
+    {"question": "What does the term Progression mean in grant assessment?",
+    "answers": {"a": "moving forward in your education",
+                "b": "moving backward in your qualifications",
+                "c": "staying at the same level"},
+    "correct_answer": "a"},
+    {"question": "How many levels are there in the QQI framework\n
+    of qualifications?",
+    "answers": {"a": "7",
+                "b": "10",
+                "c": "15"},
+    "correct_answer": "b"},
+    {"question": "What is the lowest qualification level that is\n 
+    covered by grant funding?",
+    "answers": {"a": "PLC Level 5",
+                "b": "Undergraduate Degree Level 7",
+                "c": "Leaving Certificate Level 4"},
+    "correct_answer": "a"},
+    {"question": "Which of these levels is not part of the Undergraduate levels?",
+    "answers": {"a": "Level 8 Higher Diploma",
+                "b": "Level 6 Higher Certificate",
+                "c": "Level 7 Ordinary Bachelor Degree"},
+    "correct_answer": "a"},
+    {"question": "Which of these colleges is not an approved college for grant funding?",
+    "answers": {"a": "University College Dublin",
+                "b": "Trinity College",
+                "c": "Dublin Business School"},
+    "correct_answer": "c"},
+]
